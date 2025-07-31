abc4e40dd2d6793ab1db1cf30887f308f0cca41c
Md. Abu Bakar Siddique <abms.siddique@gmail.com>
2025-07-31 18:48:07 +0600

    first commit

---
+++ src/Banner.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+function Banner() {
+  return (
+    <div className='bg-green-200 p-1 m-2 rounded-md border '>
+        <div class="text-xl py-2 text-green-700 font-bold text-center">
+            We are here to learn React from tapaScript 
+        </div>
+    </div>
+  )
+}
+
+export default Banner
